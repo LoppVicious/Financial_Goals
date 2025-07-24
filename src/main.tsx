9e8495ec6d9db9139c0561044cfb2ce2b7721dd2
Guillem Girona <43649972+LoppVicious@users.noreply.github.com>
2025-07-24 19:40:29 +0200

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import './index.css';         // ← Esto carga tu Tailwind + base styles
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-
+import { StrictMode } from 'react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
