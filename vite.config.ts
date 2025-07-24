c75ddc9dd79e30c31ee55000ffcdf32ca428b503
Guillem Girona <43649972+LoppVicious@users.noreply.github.com>
2025-07-24 16:54:16 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
-// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',   // expone en todas las IPs
-    port: 5173,        // fuerza el 5173
-    strictPort: true   // si 5173 está ocupado, falla en vez de cambiar
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),  
+        // "/api/health" → "/health"
+      }
+    }
   }
 })
