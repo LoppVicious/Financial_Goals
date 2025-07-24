fdb0c8f8d7088871db28addb02aea94343c6bcb9
Guillem Girona <43649972+LoppVicious@users.noreply.github.com>
2025-07-24 16:44:05 +0200

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',    // expone en todas las interfaces
-    port: 5173,         // fuerza el puerto
-    strictPort: true    // si 5173 está ocupado, Vite fallará en lugar de cambiar
+    host: '0.0.0.0',   // expone en todas las IPs
+    port: 5173,        // fuerza el 5173
+    strictPort: true   // si 5173 está ocupado, falla en vez de cambiar
   }
 })
